bd08d7f930a6010068e6821a45fb80e3e4309b2d
Jennifer Techel <jennifertechel@hotmail.com>
2023-12-20 09:41:41 +0100

    add custom fonts (wip)

--- client/screen/HomeScreen.tsx
+++ client/screen/HomeScreen.tsx
@@ -1,14 +1,43 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StyleSheet, View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { Button, Text, useTheme } from "react-native-paper";
 import { RootStackParamList } from "../RootNavigator";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 type Props = NativeStackScreenProps<RootStackParamList, "Home">;
 
 export default function HomeScreen({ navigation }: Props) {
+  const theme = useTheme();
+  const [isLoaded] = useFonts({
+    Oswald: require("../assets/fonts/Oswald-Bold.ttf"),
+    Overpass: require("../assets/fonts/Overpass-Light.ttf"),
+  });
+
+  const handleOnLayout = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isLoaded]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
+    <View
+      style={[styles.container, { backgroundColor: theme.colors.background }]}
+      onLayout={handleOnLayout}
+    >
+      <Text variant="displayLarge" style={styles.title}>
+        Home
+      </Text>
+      <Text variant="displayMedium" style={styles.text}>
+        Another text
+      </Text>
       <Button mode="contained" onPress={() => navigation.navigate("Login")}>
         Go to Detail
       </Button>
@@ -21,5 +50,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    fontFamily: "Oswald",
+  },
+  text: {
+    fontFamily: "Overpass",
   },
 });
---
+++ client/theme.tsx
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#A36361",
+    accent: "#735751",
+    background: "#FAF8F4",
+  },
+};
+
+export default theme;
