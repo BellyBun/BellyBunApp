5375946395eefabcc0f7d139db558942151efdca
Jennifer Techel <jennifertechel@hotmail.com>
2024-01-02 10:53:46 +0100

    add validation

--- client/screen/SignupScreen.tsx
+++ client/screen/SignupScreen.tsx
@@ -1,14 +1,30 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { StyleSheet, View } from "react-native";
-import { Button, Text, TextInput, useTheme } from "react-native-paper";
+import { GestureResponderEvent, StyleSheet, View } from "react-native";
+import {
+  Button,
+  Checkbox,
+  Text,
+  TextInput,
+  useTheme,
+} from "react-native-paper";
 import { RootStackParamList } from "../RootNavigator";
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import { useAuth } from "../context/userContext";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
-type FormData = {
-  email: string;
-  password: string;
-};
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Vänligen ange en giltig mejladress")
+    .required("Vänligen ange en mejladress"),
+  password: yup
+    .string()
+    .required("Ange ett lösenord med 8 tecken")
+    .min(8, "Lösenordet är för kort. Det behöver minst 8 tecken."),
+  acceptTerms: yup
+    .boolean()
+    .oneOf([true], "Du måste acceptera villkoren för att fortsätta"),
+});
 
 type Props = NativeStackScreenProps<RootStackParamList, "Signup">;
 
@@ -16,74 +32,61 @@ export default function SignupScreen({ navigation }: Props) {
   const theme = useTheme();
   const { signUp } = useAuth();
 
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
-
-  const onSubmit: SubmitHandler<FormData> = async (data) => {
-    try {
-      await signUp(data.email, data.password);
-      alert("Registration successfull.");
-    } catch (error) {
-      console.error("Registration error:", error);
-      alert("Registration failed. Please try again.");
-    }
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      acceptTerms: false,
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      try {
+        await signUp(values.email, values.password);
+        alert("Registration successfull.");
+      } catch (error) {
+        console.error("Registration error:", error);
+        alert("Registration failed. Please try again.");
+      }
+    },
+  });
 
   return (
     <View style={[styles.container, { backgroundColor: theme.colors.primary }]}>
       <Text variant="displaySmall" style={{ color: theme.colors.background }}>
         Skapa ett nytt konto
       </Text>
-      <Controller
-        control={control}
-        name="email"
-        rules={{ required: "Mejladress är obligatoriskt" }}
-        render={({ field }) => (
-          <>
-            <TextInput
-              label="Mejladress"
-              value={field.value}
-              onBlur={field.onBlur}
-              onChangeText={(value) => field.onChange(value)}
-              mode="outlined"
-              style={styles.input}
-            />
-            {errors.email && (
-              <Text style={{ color: "red" }}>{errors.email.message}</Text>
-            )}
-          </>
-        )}
+      <TextInput
+        label="Mejladress"
+        value={formik.values.email}
+        onBlur={formik.handleBlur("email")}
+        onChangeText={formik.handleChange("email")}
+        mode="outlined"
+        style={styles.input}
       />
+      {formik.touched.email && formik.errors.email && (
+        <Text style={{ color: "darkred" }}>{formik.errors.email}</Text>
+      )}
 
-      <Controller
-        control={control}
-        name="password"
-        rules={{ required: "Ange ett lösenord" }}
-        render={({ field }) => (
-          <>
-            <TextInput
-              label="Lösenord"
-              value={field.value}
-              onChangeText={(value) => field.onChange(value)}
-              mode="outlined"
-              secureTextEntry
-              textContentType="password"
-              style={styles.input}
-            />
-            {errors.password && (
-              <Text style={{ color: "red" }}>{errors.password.message}</Text>
-            )}
-          </>
-        )}
+      <TextInput
+        label="Lösenord"
+        value={formik.values.password}
+        onChangeText={formik.handleChange("password")}
+        mode="outlined"
+        secureTextEntry
+        textContentType="password"
+        style={styles.input}
       />
+      {formik.touched.password && formik.errors.password && (
+        <Text style={{ color: "darkred" }}>{formik.errors.password}</Text>
+      )}
 
       <Button
         mode="elevated"
         buttonColor={theme.colors.background}
-        onPress={handleSubmit(onSubmit)}
+        onPress={(e: GestureResponderEvent) => {
+          e.preventDefault();
+          formik.handleSubmit();
+        }}
       >
         Skapa konto
       </Button>
@@ -105,5 +108,9 @@ const styles = StyleSheet.create({
   input: {
     width: "60%",
     borderRadius: 100,
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
