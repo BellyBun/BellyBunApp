e03a1d81942cfe898d9017c1c7709a6738447a37
Jennifer Techel <jennifertechel@hotmail.com>
2024-01-04 22:03:16 +0100

    remove font loading and fix navigation props

--- client/screen/SignupScreen.tsx
+++ client/screen/SignupScreen.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Button, Text, TextInput, useTheme } from 'react-native-paper';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { useAuth } from '../context/userContext';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useAuth } from "../context/userContext";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../RootNavigator";
+
+type Props = NativeStackScreenProps<RootStackParamList, "Signup">;
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string().required('Password is required'),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string().required("Password is required"),
 });
 
-interface SignupScreenProps {
-  navigation: any; // Add the correct type for navigation
-}
-
-export default function SignupScreen({ navigation }: SignupScreenProps) {
+export default function SignupScreen({ navigation }: Props) {
   const theme = useTheme();
   const { signUp } = useAuth();
 
@@ -22,11 +22,11 @@ export default function SignupScreen({ navigation }: SignupScreenProps) {
     try {
       const lowercaseEmail = values.email.toLowerCase();
       await signUp(lowercaseEmail, values.password);
-      alert('Registration successful.');
-      navigation.navigate('Login');
+      alert("Registration successful.");
+      navigation.navigate("Login");
     } catch (error) {
-      console.error('Registration error:', error);
-      alert('Registration failed. Please try again.');
+      console.error("Registration error:", error);
+      alert("Registration failed. Please try again.");
     }
   };
 
@@ -36,7 +36,7 @@ export default function SignupScreen({ navigation }: SignupScreenProps) {
         Skapa ett nytt konto
       </Text>
       <Formik
-        initialValues={{ email: '', password: '' }}
+        initialValues={{ email: "", password: "" }}
         validationSchema={validationSchema}
         onSubmit={onSubmit}
       >
@@ -45,25 +45,27 @@ export default function SignupScreen({ navigation }: SignupScreenProps) {
             <TextInput
               label="Mejladress"
               value={values.email}
-              onBlur={handleBlur('email')}
-              onChangeText={handleChange('email')}
+              onBlur={handleBlur("email")}
+              onChangeText={handleChange("email")}
               mode="outlined"
               style={styles.input}
             />
-            {errors.email && <Text style={{ color: 'red' }}>{errors.email}</Text>}
+            {errors.email && (
+              <Text style={{ color: "red" }}>{errors.email}</Text>
+            )}
 
             <TextInput
               label="Lösenord"
               value={values.password}
-              onBlur={handleBlur('password')}
-              onChangeText={handleChange('password')}
+              onBlur={handleBlur("password")}
+              onChangeText={handleChange("password")}
               mode="outlined"
               secureTextEntry
               textContentType="password"
               style={styles.input}
             />
             {errors.password && (
-              <Text style={{ color: 'red' }}>{errors.password}</Text>
+              <Text style={{ color: "red" }}>{errors.password}</Text>
             )}
 
             <Button
@@ -77,7 +79,7 @@ export default function SignupScreen({ navigation }: SignupScreenProps) {
         )}
       </Formik>
 
-      <Button mode="elevated" onPress={() => navigation.navigate('Home')}>
+      <Button mode="elevated" onPress={() => navigation.navigate("Home")}>
         Go to Home
       </Button>
     </View>
@@ -87,13 +89,13 @@ export default function SignupScreen({ navigation }: SignupScreenProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
     gap: 20,
   },
   input: {
-    width: '60%',
+    width: "60%",
     borderRadius: 100,
   },
 });
--- client/screen/UserInfoScreen.tsx
+++ client/screen/UserInfoScreen.tsx
@@ -1,50 +1,29 @@
-import React, { useCallback } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Button, Text, TextInput, useTheme } from 'react-native-paper';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { useAuth } from '../context/userContext';
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useAuth } from "../context/userContext";
 import { RootStackParamList } from "../RootNavigator";
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-
-
-SplashScreen.preventAutoHideAsync();
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Home">;
 
 const validationSchema = Yup.object().shape({
-  userName: Yup.string().required('Username is required'),
-  newEmail: Yup.string().email('Invalid email').required('Email is required'),
+  userName: Yup.string().required("Username is required"),
+  newEmail: Yup.string().email("Invalid email").required("Email is required"),
 });
 
 export default function UserInfoScreen({ navigation }: Props) {
   const theme = useTheme();
   const { user, addUserInfo } = useAuth();
-  const [isLoaded] = useFonts({
-    Oswald: require("../assets/fonts/Oswald-Bold.ttf"),
-    Overpass: require("../assets/fonts/Overpass-Light.ttf"),
-  });
-
-  const handleOnLayout = useCallback(async () => {
-    if (isLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [isLoaded]);
-
-  if (!isLoaded) {
-    return (
-      <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
-        <Text>Loading...</Text>
-      </View>
-    );
-  }
 
   // Render loading state while user data is being fetched
   if (user === null) {
     return (
-      <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
+      <View
+        style={[styles.container, { backgroundColor: theme.colors.background }]}
+      >
         <Text>Loading...</Text>
       </View>
     );
@@ -53,11 +32,13 @@ export default function UserInfoScreen({ navigation }: Props) {
   // Check if the user is logged in
   if (!user) {
     return (
-      <View style={[styles.container, { backgroundColor: theme.colors.primary }]}>
+      <View
+        style={[styles.container, { backgroundColor: theme.colors.primary }]}
+      >
         <Text variant="displaySmall" style={{ color: theme.colors.background }}>
           User not logged in
         </Text>
-        <Button mode="elevated" onPress={() => navigation.navigate('Home')}>
+        <Button mode="elevated" onPress={() => navigation.navigate("Home")}>
           Go to Home
         </Button>
       </View>
@@ -68,21 +49,24 @@ export default function UserInfoScreen({ navigation }: Props) {
     try {
       const userId = user.id;
       await addUserInfo(userId, values);
-      alert('User information updated successfully.');
+      alert("User information updated successfully.");
       // You can navigate or perform additional actions after updating user info
     } catch (error) {
-      console.error('User info update error:', error);
-      alert('Failed to update user information. Please try again.');
+      console.error("User info update error:", error);
+      alert("Failed to update user information. Please try again.");
     }
   };
 
   return (
-    <View style={[styles.container, { backgroundColor: theme.colors.primary }]} onLayout={handleOnLayout}>
+    <View style={[styles.container, { backgroundColor: theme.colors.primary }]}>
       <Text variant="displaySmall" style={{ color: theme.colors.background }}>
         Update User Info
       </Text>
       <Formik
-        initialValues={{ userName: user.userName || '', newEmail: user.email || '' }}
+        initialValues={{
+          userName: user.userName || "",
+          newEmail: user.email || "",
+        }}
         validationSchema={validationSchema}
         onSubmit={onSubmit}
       >
@@ -91,22 +75,26 @@ export default function UserInfoScreen({ navigation }: Props) {
             <TextInput
               label="Ditt namn"
               value={values.userName}
-              onBlur={handleBlur('userName')}
-              onChangeText={handleChange('userName')}
+              onBlur={handleBlur("userName")}
+              onChangeText={handleChange("userName")}
               mode="outlined"
               style={styles.input}
             />
-            {errors.userName && <Text style={{ color: 'red' }}>{errors.userName}</Text>}
+            {errors.userName && (
+              <Text style={{ color: "red" }}>{errors.userName}</Text>
+            )}
 
             <TextInput
               label="Ny Email"
               value={values.newEmail}
-              onBlur={handleBlur('newEmail')}
-              onChangeText={handleChange('newEmail')}
+              onBlur={handleBlur("newEmail")}
+              onChangeText={handleChange("newEmail")}
               mode="outlined"
               style={styles.input}
             />
-            {errors.newEmail && <Text style={{ color: 'red' }}>{errors.newEmail}</Text>}
+            {errors.newEmail && (
+              <Text style={{ color: "red" }}>{errors.newEmail}</Text>
+            )}
 
             <Button
               mode="elevated"
@@ -119,11 +107,14 @@ export default function UserInfoScreen({ navigation }: Props) {
         )}
       </Formik>
 
-      <Button mode="elevated" onPress={() => navigation.navigate('AddPregnancy')}>
+      <Button
+        mode="elevated"
+        onPress={() => navigation.navigate("AddPregnancy")}
+      >
         Add Pregnancy
       </Button>
 
-      <Button mode="elevated" onPress={() => navigation.navigate('Home')}>
+      <Button mode="elevated" onPress={() => navigation.navigate("Home")}>
         Go to Home
       </Button>
     </View>
@@ -133,13 +124,13 @@ export default function UserInfoScreen({ navigation }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
     gap: 20,
   },
   input: {
-    width: '60%',
+    width: "60%",
     borderRadius: 100,
   },
 });
