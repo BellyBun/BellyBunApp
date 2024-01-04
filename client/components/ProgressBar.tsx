a95be6fa271921368512d7eb7fff5687a50b35bc
Martina Kreivi <martinakreivi@gmail.com>
2024-01-04 19:18:43 +0100

    styling on circle

--- client/components/ProgressBar.tsx
+++ client/components/ProgressBar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { ProgressBar } from "react-native-paper";
+import { Circle, Svg } from "react-native-svg";
 
 const PregnancyProgress = () => {
   const [progress, setProgress] = useState(0);
@@ -20,31 +20,46 @@ const PregnancyProgress = () => {
 
     const calculatedProgress =
       (gestationalAgeInWeeks / totalWeeksOfPregnancy) * 100;
-    console.log("Gestational Age (weeks):", gestationalAgeInWeeks);
-    console.log("Calculated Progress:", calculatedProgress);
 
     const clampedProgress = Math.min(100, Math.max(0, calculatedProgress));
-    console.log("Clamped Progress:", clampedProgress);
 
     setProgress(clampedProgress);
   }, []);
 
+  const radius = 90; // set the radius of the circle
+
+  // Calculate the circumference of the circle
+  const circumference = 2 * Math.PI * radius;
+
+  // Calculate the stroke-dashoffset based on the progress
+  const strokeDashoffset = (progress / 100) * circumference;
+
   return (
     <View style={styles.container}>
       <Text style={styles.progressText}>
-        Pregnancy Progress: {progress.toFixed(2)}%
+        {progress.toFixed(2)}% av graviditeten är avklarad!
       </Text>
-      <ProgressBar
-        progress={progress / 100}
-        color="#735751"
-        style={{ height: 10 }}
-      />
+      <Svg height={2 * radius} width={2 * radius}>
+        <Circle
+          cx={radius}
+          cy={radius}
+          r={radius - 30} // bredden på cirkeln
+          fill="none"
+          stroke="#735751"
+          strokeWidth="10"
+          strokeDasharray={circumference}
+          strokeDashoffset={strokeDashoffset}
+          transform="rotate(-90, 80, 80)"
+        />
+      </Svg>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    justifyContent: "center",
     padding: 16,
   },
   progressText: {
