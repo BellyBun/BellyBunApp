22b279c0f08f7e35de528d96f8d7a906d16eb850
Jennifer Techel <jennifertechel@hotmail.com>
2024-01-10 10:06:17 +0100

    add userrouter and remove cookie

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,12 @@
-import express, { RequestHandler } from "express";
+import express from "express";
 import "express-async-errors";
-import { cookieSessionMiddleware } from "./cookieSession";
 import { errorHandler } from "./errorHandler";
+import userRouter from "./routes/user-routes";
 
 export const app = express();
 
 app.use(express.json());
 
-app.use(cookieSessionMiddleware as RequestHandler);
+app.use(userRouter);
 
 app.use(errorHandler);
