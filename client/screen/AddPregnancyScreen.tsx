ab00ed5c6a2424f1d7d5cf53e666a128accc21d2
Jennifer Techel <jennifertechel@hotmail.com>
2024-01-15 13:34:36 +0100

    add pregnancy from context

--- client/screen/AddPregnancyScreen.tsx
+++ client/screen/AddPregnancyScreen.tsx
@@ -4,9 +4,9 @@ import { Button, Text, TextInput, useTheme } from "react-native-paper";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import { useUser } from "../context/userContext";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SettingsStackParamList } from "../RootNavigator";
+import { useBaby } from "../context/babyContext";
 
 type Props = NativeStackScreenProps<SettingsStackParamList, "AddPregnancy">;
 
@@ -19,7 +19,7 @@ const validationSchema = Yup.object().shape({
 
 const AddPregnancyScreen = ({ navigation }: Props) => {
   const theme = useTheme();
-  const { user } = useUser();
+  const { createPregnancy } = useBaby();
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
   const showDatePicker = () => {
@@ -30,21 +30,15 @@ const AddPregnancyScreen = ({ navigation }: Props) => {
     setDatePickerVisibility(false);
   };
 
-  //   const handleConfirm = (date: Date) => {
-  //     setDueDate(date);
-  //     hideDatePicker();
-  //   };
-
   const onSubmit = async (values: { babyName: string; dueDate: Date }) => {
-    // try {
-    //   await addPregnancy(user?.id || "", values.babyName, values.dueDate);
-    //   console.log("Pregnancy added successfully");
-    //   // navigation.replace("Home");
-    //   // You can navigate or perform additional actions after adding pregnancy
-    // } catch (error) {
-    //   console.error("Add pregnancy error:", error);
-    //   alert("Failed to add pregnancy. Please try again.");
-    // }
+    try {
+      await createPregnancy(values.babyName, values.dueDate);
+      console.log("Pregnancy added successfully");
+      alert("Pregnancy successfully added");
+    } catch (error) {
+      console.error("Add pregnancy error:", error);
+      alert("Failed to add pregnancy. Please try again.");
+    }
   };
 
   return (
