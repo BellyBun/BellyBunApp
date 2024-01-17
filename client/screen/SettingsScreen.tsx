157b274bcd1c2cad36691fdad7cb00cdcadb6453
Jennifer Techel <jennifertechel@hotmail.com>
2024-01-17 10:52:35 +0100

    add some styling

--- client/screen/SettingsScreen.tsx
+++ client/screen/SettingsScreen.tsx
@@ -1,15 +1,15 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SafeAreaView, StyleSheet, TouchableOpacity, View } from "react-native";
-import { Button, Text, useTheme } from "react-native-paper";
+import { Button, Text } from "react-native-paper";
 import { SettingsStackParamList } from "../RootNavigator";
 import { useState } from "react";
 import { useUser } from "../context/userContext";
 import { useBaby } from "../context/babyContext";
+import theme from "../theme";
 
 type Props = NativeStackScreenProps<SettingsStackParamList, "Settings">;
 
 export default function SettingsScreen({ navigation }: Props) {
-  const theme = useTheme();
   const [isFirstExpanded, setFirstExpanded] = useState(false);
   const [isSecondExpanded, setSecondExpanded] = useState(false);
   const { signout } = useUser();
@@ -32,30 +32,25 @@ export default function SettingsScreen({ navigation }: Props) {
     }
   };
   return (
-    <SafeAreaView
-      style={[styles.safeContainer, { backgroundColor: theme.colors.primary }]}
-    >
+    <SafeAreaView style={styles.safeContainer}>
       <View style={styles.container}>
-        <Text
-          variant="displayMedium"
-          style={[styles.title, { color: theme.colors.background }]}
-        >
+        <Text variant="displayMedium" style={styles.title}>
           MITT KONTO
         </Text>
 
         {/* Graviditeter Accordion */}
         <TouchableOpacity onPress={toggleFirstAccordion} activeOpacity={0.8}>
           <View style={styles.accordionTitleContainer}>
-            <Text
-              variant="headlineMedium"
-              style={[styles.listTitle, { color: theme.colors.background }]}
-            >
+            <Text variant="headlineMedium" style={styles.listTitle}>
               GRAVIDITETER
             </Text>
           </View>
         </TouchableOpacity>
         {isFirstExpanded && babies.length > 0 && (
           <>
+            <Text variant="titleLarge" style={styles.smallTitle}>
+              VÄLJ BEBIS
+            </Text>
             {babies.map((baby) => (
               <Button
                 key={baby._id}
@@ -82,10 +77,7 @@ export default function SettingsScreen({ navigation }: Props) {
         {/* Inställningar Accordion */}
         <TouchableOpacity onPress={toggleSecondAccordion} activeOpacity={0.8}>
           <View style={styles.accordionTitleContainer}>
-            <Text
-              variant="headlineMedium"
-              style={[styles.listTitle, { color: theme.colors.background }]}
-            >
+            <Text variant="headlineMedium" style={styles.listTitle}>
               INSTÄLLNINGAR
             </Text>
           </View>
@@ -124,24 +116,32 @@ const styles = StyleSheet.create({
   safeContainer: {
     flex: 1,
     width: "100%",
+    backgroundColor: theme.colors.primary,
   },
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     gap: 20,
+    paddingTop: "20%",
     width: "100%",
   },
   title: {
     fontFamily: "Oswald",
+    color: theme.colors.background,
+  },
+  smallTitle: {
+    fontFamily: "Oswald",
+    color: theme.colors.background,
   },
   accordionTitleContainer: {
     width: "100%",
-    paddingVertical: 12,
+    paddingVertical: 10,
   },
   listTitle: {
     fontFamily: "Oswald",
     width: "100%",
+    color: theme.colors.background,
   },
   listButton: {
     marginTop: 5,
@@ -150,6 +150,7 @@ const styles = StyleSheet.create({
   button: {
     borderBottomWidth: 1,
     borderBottomColor: "white",
-    fontSize: 60,
+    position: "absolute",
+    bottom: 50,
   },
 });
